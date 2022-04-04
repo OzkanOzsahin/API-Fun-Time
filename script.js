74c341b1ada73c5afe76ed73378370212ca446a1
OzkanOzsahin <92115875+OzkanOzsahin@users.noreply.github.com>
2022-04-04 11:44:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//html element.
+const button = document.querySelector(`.container button`);
+const jokeText = document.querySelector(`.container p`);
+document.addEventListener(`DOMContentLoaded`,getJoke)
+ //Add click button
+button.addEventListener('click', getJoke);
+//fectch url
+ async function getJoke()  {
+ const jokeData = await fetch(`https://icanhazdadjoke.com/ `,{
+     headers:{
+        "Accept": "application/json"
+     }
+ });
+ //await json
+ const jokeObj = await jokeData.json();
+ jokeText.innerHTML = jokeObj.joke;
+
+}
